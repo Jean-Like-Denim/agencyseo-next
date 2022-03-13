b52972059747df3b805bcc5c3d359cef4d599405
Jean-Like-Denim <jean@ponderlust.xyz>
2022-03-13 14:43:38 +0000

    New Pages

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://agencyseo.com.au">Agency SEO</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Looking to Automate your workflow? <br></br>
+          Drop in your root domain and find out how easy your job can be.{' '}
+          <code className={styles.input}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -27,26 +28,26 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://agencyseo.com.au/learn-seo" className={styles.card}>
+            <h2>Learn SEO &rarr;</h2>
+            <p>Get ahead of the competition with our automation tool. Be untouchable with our knowledge base.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://agencyseo.com.au/how-it-works"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>How It Works &rarr;</h2>
+            <p>Get everything you need to set up your client manually, or automate your workflow with our integrations..</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://agencyseo.com.au/get-started"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Get Started &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Get three months of set up, research and optimisation—done in 3 seconds.
             </p>
           </a>
         </div>
