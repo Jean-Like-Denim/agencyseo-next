01058ef026a2a4d8f4138fea35c30c848685a51e
Jean-Like-Denim <jean@ponderlust.xyz>
2022-03-18 12:13:46 +0000

    setup from youtube vid

---
+++ pages/get-started.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class getStarted extends Component {
+  render() {
+    return (
+        <div>
+            <head>
+                <title></title>
+                </head>
+                <h1>Get Started</h1></div>
+    )
+  }
+}
---
+++ pages/learn-seo.js
@@ -0,0 +1,9 @@
+const learnSeo = () => {
+    return (
+        <div>
+            <h1>Learn SEO</h1>
+        </div>
+    )
+} 
+
+export default learnSeo
